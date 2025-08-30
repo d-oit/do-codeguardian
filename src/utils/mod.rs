6a7bfe34161f29db60094686af6e281f18d5cb19
Dominik Oswald <6849456+d-oit@users.noreply.github.com>
2025-08-30 15:34:53 +0000

    Revert "Initial commit"
    This reverts commit 443a38b56db1d14ce39208554c66950228e37e8d.

--- src/utils/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod git;
-pub mod progress;
-pub mod security;
-
-// Performance-related modules have been moved to src/performance/
-// Re-export them for backward compatibility
-pub use crate::performance::{caching as memory_pool, parallelism as adaptive_parallelism};
